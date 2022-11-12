02685d74e8b53e9e31ae02e6df1c010acd2c3270
GustavoFerMartins <60547697+Gustavo3022020@users.noreply.github.com>
2022-11-12 09:36:59 -0300

    CustomWorldCup

---
+++ CopaDoMundo/src/code/botao.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package code;
+
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import javax.swing.JButton;
+import javax.swing.border.EmptyBorder;
+       
+public class botao extends JButton {
+    public botao() {
+        setContentAreaFilled(false);
+        setBorder(new EmptyBorder(5, 0, 5, 0));
+        setBackground(Color.WHITE);
+        setCursor(new Cursor(Cursor.HAND_CURSOR));
+    }
+   
+    protected void paintComponent(Graphics g) {
+        int widht = getWidth();
+        int height = getHeight();
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.drawRoundRect(0, 0, widht - 1, height - 1, height, height);
+        super.paintComponent(g);
+    }
+}
